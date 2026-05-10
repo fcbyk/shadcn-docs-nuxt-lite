91228af5ad91ca8f8cf39ec5b83b334ba5df342e
fcbyk <731240932@qq.com>
2026-05-11 00:15:50 +0800

    isolate starter workspace for local package testing

--- starter/nuxt.config.ts
+++ starter/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  extends: ['shadcn-docs-nuxt'],
+  extends: ['@fcbyk/shadcn-docs-nuxt-lite'],
   i18n: {
     defaultLocale: 'en',
     locales: [
